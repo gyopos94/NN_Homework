d56a4eab55e71cc7d6b37db237abf6ec4d9cae64
Marcell Gyopos <marcellgyopos@gmail.com>
2024-02-05 14:20:57 +0100

    Add negative test cases

--- src/test/java/com/nn/homework/service/JobLaunchingServiceTest.java
+++ src/test/java/com/nn/homework/service/JobLaunchingServiceTest.java
@@ -1,13 +1,15 @@
 package com.nn.homework.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 
-import com.nn.homework.exception.InvalidJobParameterException;
+import com.nn.homework.exception.JobLaunchException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,7 +18,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 
 @ExtendWith(MockitoExtension.class)
 class JobLaunchingServiceTest {
@@ -68,13 +74,53 @@ class JobLaunchingServiceTest {
   }
 
   @Test
-  void throwInvalidJobParameterExceptionForUnknownJob() {
-    // Arrange & Act
-    Exception exception = assertThrows(InvalidJobParameterException.class,
-        () -> jobLaunchingService.launchJob("UnknownJob"));
+  void testLaunchJobWithJobExecutionAlreadyRunningException()
+      throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+    doThrow(new JobExecutionAlreadyRunningException("The job is already running.")).when(jobLauncher).run(any(Job.class), any());
 
-    // Verify
-    assertTrue(exception.getMessage().contains("Invalid job parameter"));
-    verifyNoInteractions(jobLauncher);
+    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("OutPayHeader"));
+    assertEquals("JobExecutionAlreadyRunningException: The job is already running.", exception.getMessage());
+  }
+
+  @Test
+  void testLaunchJobWithJobRestartException()
+      throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+    doThrow(new JobRestartException("A failure occurred in restarting the job.")).when(jobLauncher).run(any(Job.class), any());
+
+    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("Policy"));
+    assertEquals("JobRestartException: A failure occurred in restarting the job.", exception.getMessage());
+  }
+
+  @Test
+  void testLaunchJobWithJobInstanceAlreadyCompleteException()
+      throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+    doThrow(new JobInstanceAlreadyCompleteException("The job has been already completed for the given parameters.")).when(jobLauncher).run(any(Job.class), any());
+
+    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("SurValue"));
+    assertEquals("JobInstanceAlreadyCompleteException: The job has been already completed for the given parameters.", exception.getMessage());
+  }
+
+  @Test
+  void testLaunchJobWithJobParametersInvalidException()
+      throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+    doThrow(new JobParametersInvalidException("The parameters provided for the job are invalid.")).when(jobLauncher).run(any(Job.class), any());
+
+    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("OutPayHeader"));
+    assertEquals("JobParametersInvalidException: The parameters provided for the job are invalid.", exception.getMessage());
+  }
+
+  @Test
+  void testLaunchJobWithGenericException()
+      throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+    doThrow(new RuntimeException("An unexpected error occurred while running the job.")).when(jobLauncher).run(any(Job.class), any());
+
+    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("Policy"));
+    assertEquals("Exception: An unexpected error occurred while running the job.", exception.getMessage());
+  }
+
+  @Test
+  void testLaunchJobWithInvalidJobParameter() {
+    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("InvalidJobName"));
+    assertEquals("Invalid job parameter: InvalidJobName", exception.getMessage());
   }
 }
