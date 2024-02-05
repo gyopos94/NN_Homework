d77adedd5cc9773bd59cfe7fdb0fc8c5b96a94a2
Marcell Gyopos <marcellgyopos@gmail.com>
2024-02-05 16:45:47 +0100

    Adjust Tests

--- src/test/java/com/nn/homework/controller/JobControllerTest.java
+++ src/test/java/com/nn/homework/controller/JobControllerTest.java
@@ -1,7 +1,7 @@
 package com.nn.homework.controller;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -11,13 +11,13 @@ import com.nn.homework.exception.JobLaunchException;
 import com.nn.homework.service.JobLaunchingService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.batch.core.launch.JobLauncher;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(JobController.class)
@@ -29,24 +29,31 @@ class JobControllerTest {
   @MockBean
   private JobLaunchingService jobLaunchingService;
 
-
   @MockBean
   private JobLauncher jobLauncher;
 
   @Test
   void whenLaunchJobSuccessfully_thenReturnsSuccessMessage() throws Exception {
-    doNothing().when(jobLaunchingService).launchJob(anyString());
-    mockMvc.perform(get("/launch-job/{jobName}", "SurValue")
+    when(jobLaunchingService.launchJob(anyString())).thenReturn(false); // Simulate successful job launch
+    mockMvc.perform(get("/launch-job").param("jobName", "SurValue")
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(content().string("Job launched successfully."));
   }
 
   @Test
-  void whenLaunchJobFails_thenReturnsErrorMessage() throws Exception {
-    doThrow(new JobLaunchException("Error launching job", null)).when(jobLaunchingService)
-        .launchJob(anyString());
-    mockMvc.perform(get("/launch-job/{jobName}", "invalidJobName")
+  void whenLaunchJobFails_thenReturnsFailureMessage() throws Exception {
+    when(jobLaunchingService.launchJob(anyString())).thenReturn(true); // Simulate failed job launch
+    mockMvc.perform(get("/launch-job").param("jobName", "invalidJobName")
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isInternalServerError())
+        .andExpect(content().string("Job failed to complete successfully."));
+  }
+
+  @Test
+  void whenLaunchJobThrowsException_thenReturnsErrorMessage() throws Exception {
+    doThrow(new JobLaunchException("Error launching job", null)).when(jobLaunchingService).launchJob(anyString());
+    mockMvc.perform(get("/launch-job").param("jobName", "exceptionJobName")
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isInternalServerError())
         .andExpect(content().string("Error launching job: Error launching job"));
--- src/test/java/com/nn/homework/service/JobLaunchingServiceTest.java
+++ src/test/java/com/nn/homework/service/JobLaunchingServiceTest.java
@@ -1,13 +1,13 @@
 package com.nn.homework.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 import com.nn.homework.exception.JobLaunchException;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +16,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -48,79 +50,108 @@ class JobLaunchingServiceTest {
 
   @Test
   void launchOutPayHeaderJobSuccessfully() throws Exception {
-    // Act
-    jobLaunchingService.launchJob("OutPayHeader");
+    JobExecution jobExecution = new JobExecution(1L);
+    jobExecution.setStatus(BatchStatus.COMPLETED);
+    when(jobLauncher.run(eq(outPayHeaderJob), any(JobParameters.class))).thenReturn(jobExecution);
 
-    // Verify
+    boolean isUnsuccessful = jobLaunchingService.launchJob("OutPayHeader");
+
+    assertFalse(isUnsuccessful);
     verify(jobLauncher).run(eq(outPayHeaderJob), any(JobParameters.class));
   }
 
   @Test
   void launchPolicyJobSuccessfully() throws Exception {
-    // Act
-    jobLaunchingService.launchJob("Policy");
+    JobExecution jobExecution = new JobExecution(1L);
+    jobExecution.setStatus(BatchStatus.COMPLETED);
+    when(jobLauncher.run(eq(policyJob), any(JobParameters.class))).thenReturn(jobExecution);
 
-    // Verify
+    boolean isUnsuccessful = jobLaunchingService.launchJob("Policy");
+
+    assertFalse(isUnsuccessful);
     verify(jobLauncher).run(eq(policyJob), any(JobParameters.class));
   }
 
   @Test
   void launchSurValueJobSuccessfully() throws Exception {
-    // Act
-    jobLaunchingService.launchJob("SurValue");
+    JobExecution jobExecution = new JobExecution(1L);
+    jobExecution.setStatus(BatchStatus.COMPLETED);
+    when(jobLauncher.run(eq(surValueJob), any(JobParameters.class))).thenReturn(jobExecution);
 
-    // Verify
+    boolean isUnsuccessful = jobLaunchingService.launchJob("SurValue");
+
+    assertFalse(isUnsuccessful);
     verify(jobLauncher).run(eq(surValueJob), any(JobParameters.class));
   }
+
 
   @Test
   void testLaunchJobWithJobExecutionAlreadyRunningException()
       throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-    doThrow(new JobExecutionAlreadyRunningException("The job is already running.")).when(jobLauncher).run(any(Job.class), any());
+    doThrow(new JobExecutionAlreadyRunningException("The job is already running.")).when(
+        jobLauncher).run(any(Job.class), any());
 
-    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("OutPayHeader"));
-    assertEquals("JobExecutionAlreadyRunningException: The job is already running.", exception.getMessage());
+    JobLaunchException exception = assertThrows(JobLaunchException.class,
+        () -> jobLaunchingService.launchJob("OutPayHeader"));
+    assertEquals("JobExecutionAlreadyRunningException: The job is already running.",
+        exception.getMessage());
   }
 
   @Test
   void testLaunchJobWithJobRestartException()
       throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-    doThrow(new JobRestartException("A failure occurred in restarting the job.")).when(jobLauncher).run(any(Job.class), any());
+    doThrow(new JobRestartException("A failure occurred in restarting the job.")).when(jobLauncher)
+        .run(any(Job.class), any());
 
-    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("Policy"));
-    assertEquals("JobRestartException: A failure occurred in restarting the job.", exception.getMessage());
+    JobLaunchException exception = assertThrows(JobLaunchException.class,
+        () -> jobLaunchingService.launchJob("Policy"));
+    assertEquals("JobRestartException: A failure occurred in restarting the job.",
+        exception.getMessage());
   }
 
   @Test
   void testLaunchJobWithJobInstanceAlreadyCompleteException()
       throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-    doThrow(new JobInstanceAlreadyCompleteException("The job has been already completed for the given parameters.")).when(jobLauncher).run(any(Job.class), any());
+    doThrow(new JobInstanceAlreadyCompleteException(
+        "The job has been already completed for the given parameters.")).when(jobLauncher)
+        .run(any(Job.class), any());
 
-    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("SurValue"));
-    assertEquals("JobInstanceAlreadyCompleteException: The job has been already completed for the given parameters.", exception.getMessage());
+    JobLaunchException exception = assertThrows(JobLaunchException.class,
+        () -> jobLaunchingService.launchJob("SurValue"));
+    assertEquals(
+        "JobInstanceAlreadyCompleteException: The job has been already completed for the given parameters.",
+        exception.getMessage());
   }
 
   @Test
   void testLaunchJobWithJobParametersInvalidException()
       throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-    doThrow(new JobParametersInvalidException("The parameters provided for the job are invalid.")).when(jobLauncher).run(any(Job.class), any());
+    doThrow(
+        new JobParametersInvalidException("The parameters provided for the job are invalid.")).when(
+        jobLauncher).run(any(Job.class), any());
 
-    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("OutPayHeader"));
-    assertEquals("JobParametersInvalidException: The parameters provided for the job are invalid.", exception.getMessage());
+    JobLaunchException exception = assertThrows(JobLaunchException.class,
+        () -> jobLaunchingService.launchJob("OutPayHeader"));
+    assertEquals("JobParametersInvalidException: The parameters provided for the job are invalid.",
+        exception.getMessage());
   }
 
   @Test
   void testLaunchJobWithGenericException()
       throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-    doThrow(new RuntimeException("An unexpected error occurred while running the job.")).when(jobLauncher).run(any(Job.class), any());
+    doThrow(new RuntimeException("An unexpected error occurred while running the job.")).when(
+        jobLauncher).run(any(Job.class), any());
 
-    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("Policy"));
-    assertEquals("Exception: An unexpected error occurred while running the job.", exception.getMessage());
+    JobLaunchException exception = assertThrows(JobLaunchException.class,
+        () -> jobLaunchingService.launchJob("Policy"));
+    assertEquals("Exception: An unexpected error occurred while running the job.",
+        exception.getMessage());
   }
 
   @Test
   void testLaunchJobWithInvalidJobParameter() {
-    JobLaunchException exception = assertThrows(JobLaunchException.class, () -> jobLaunchingService.launchJob("InvalidJobName"));
+    JobLaunchException exception = assertThrows(JobLaunchException.class,
+        () -> jobLaunchingService.launchJob("InvalidJobName"));
     assertEquals("Invalid job parameter: InvalidJobName", exception.getMessage());
   }
 }
