f5549ff336f96e3fc1985ce89ab0a0e7df062824
Marcell Gyopos <marcellgyopos@gmail.com>
2024-02-05 11:54:31 +0100

    Add CommandLineRunner for init testing

--- src/main/java/com/nn/homework/HomeworkApplication.java
+++ src/main/java/com/nn/homework/HomeworkApplication.java
@@ -1,13 +1,46 @@
 package com.nn.homework;
 
+
+import java.util.Map;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class HomeworkApplication {
+public class HomeworkApplication implements CommandLineRunner {
+
+  @Autowired
+  private JobLauncher jobLauncher;
+
+  @Autowired
+  private ApplicationContext applicationContext;
 
   public static void main(String[] args) {
-    SpringApplication.run(HomeworkApplication.class, args);
+    SpringApplication.run(HomeworkApplication.class, args);}
+
+  @Override
+  public void run(String... args) throws Exception {
+    Map<String, Job> jobs = applicationContext.getBeansOfType(Job.class);
+
+    jobs.forEach((beanName, job) -> {
+      try {
+        JobParameters jobParameters = new JobParametersBuilder()
+            .addLong("time", System.currentTimeMillis())
+            .toJobParameters();
+        jobLauncher.run(job, jobParameters);
+      } catch (JobExecutionException e) {
+        System.out.println("Failed to execute job " + beanName);
+        e.printStackTrace();
+      }
+    });
   }
+
 
 }
